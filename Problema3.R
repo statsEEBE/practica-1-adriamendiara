1c681a9fe72cdd172c6e4292db68404eb8f61e90
adria.mendiara <adria.mendiara@auleseebe.upc.edu>
2024-09-12 12:04:37 +0200

    error en el vector

--- Problema3.R
+++ Problema3.R
@@ -1,1 +1,9 @@
 #Codigo para problema 3
+
+x <- c(-8,5,2,-8,9,5,2,-3,1,-1,4,-4,9,3,-9,7,0,-7,8,-4,1,7,-6,4,5,-9,-2,-8,5,-5) # c es la concatenacion de datos
+sum(x)
+
+y <- x[-c(21,2,26)] # nuevo vector basado en x sin las posiciones 21, 2 y 26
+
+sum(exp(x))-sum(exp(y))
+y[c(19,24)]
